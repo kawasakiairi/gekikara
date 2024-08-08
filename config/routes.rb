0cda43fb27d6626b45aa4fca25fa030707b3e445
kawasakiairi <hgehw33034@yahoo.co.jp>
2024-08-09 00:59:46 +0900

    食品検索の流れ確定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root "foods#index"
-  resources :foods
+  root "foods#search"
+
+  resources :foods, only: %i[index] do
+    collection do
+      get :search
+    end
+  end
 end
+
