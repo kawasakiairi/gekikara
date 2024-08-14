ee0790d68144236826cb38a3cad9d43434730e81
kawasakiairi <hgehw33034@yahoo.co.jp>
2024-08-15 03:08:48 +0900

    レイアウト調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
+  skip_before_action :require_login, only: %i[new create show]
 
   # ユーザー登録ページ（8/12追加）
   def new
